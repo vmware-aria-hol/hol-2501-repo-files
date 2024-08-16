6e7130db9e38f3ac382878dc4bcd559cba7d4555
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2024-08-17 00:09:44 +0100

    Updated

--- cleanup.ps1
+++ cleanup.ps1
@@ -7,3 +7,14 @@ Get-ChildItem -path "c:\gitlab" -Directory -Recurse -Filter ".git" -Hidden | Rem
 Get-ChildItem -path "c:\gitlab" -Recurse -Filter ".gitignore" -Hidden | Remove-Item -Force -Recurse
 Get-ChildItem -path "c:\gitlab" -Recurse -Filter "README.md" -Hidden | Remove-Item -Force -Recurse
 
+
+$versionInfo = @"
+v0.21 
+HOL 2501 CMP vPod 
+2024/08/16 
+"@
+
+If (Test-Path -Path "c:\hol\version.txt" ) {
+    Remove-Item "c:\hol\version.txt" -Force
+    New-Item "c:\hol\version.txt" -ItemType File -Value $versionInfo
+}
